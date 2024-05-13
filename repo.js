6cfc79fd013070b427cfc09bb840a85cf558f172
Mikael <mikael11manukyan@gmail.com>
2024-05-13 19:52:58 +0200

    pridat zmeny do javascriptu po druhe

--- repo.js
+++ repo.js
@@ -8,3 +8,22 @@ for (let index = 0; index < seznam.length; index++) {
     const element = seznam[index];
     document.write(index + 1 + ": " + element + "</br>")
 } 
+
+let trojuhelnik = {
+    rameno : 5,
+    zakladna : 10
+}
+
+document.write("Rameno trojuhelniku" + ":" + trojuhelnik.rameno + "<br>")
+document.write("Zakladna trojuhelniku" + ":" + trojuhelnik.zakladna + "<br>")
+
+function obsahtrojuhelniku(rameno, zakladna) {
+    return (vyska * podstava) / 2
+}
+
+function obvodtrojuhelniku(rameno, zakladna) {
+    return rameno * 2 + zakladna
+}
+
+document.write("Obsah trojuhelniku" + ":" + obsahtrojuhelniku())
+document.write("Obvod trojuhelniku" + ":" + obvodtrojuhelniku)
