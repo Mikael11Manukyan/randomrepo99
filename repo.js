8ab2e2b21ec98659b4bc58a8496be136967e72a1
Mikael <mikael11manukyan@gmail.com>
2024-05-13 19:59:02 +0200

    pridat zmeny do js po 4

--- repo.js
+++ repo.js
@@ -25,5 +25,5 @@ function obvodtrojuhelniku(rameno, zakladna) {
     return rameno * 2 + zakladna
 }
 
-document.write("Obsah trojuhelniku :" + obsahtrojuhelniku())
-document.write("Obvod trojuhelniku :" + obvodtrojuhelniku())
+document.write("Obsah trojuhelniku :" + obsahtrojuhelniku(trojuhelnik.rameno, trojuhelnik.zakladna))
+document.write("Obvod trojuhelniku :" + obvodtrojuhelniku(trojuhelnik.rameno, trojuhelnik.zakladna))
